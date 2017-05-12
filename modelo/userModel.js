f30f6400d30835e222c77462e1bad4deea328c87
ANDRÉS CAMILO RODRÍGUEZ ESPITIA <ANDRS CAMILO RODRGUEZ ESPITIA>
2017-05-12 09:48:16 -0500

    add product

--- modelo/userModel.js
+++ modelo/userModel.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
 
 var userSchema = new Schema({
   username_user:String,
--- routes/category.js
+++ routes/category.js
@@ -3,40 +3,40 @@ var router = express.Router();
 var Cat = require('../modelo/categoryModel.js');
 
 
-router.get('/createCategory', function(req, res, next) {  
+router.get('/createCategory', function (req, res, next) {
   var item = {
     id_category: req.query.id_category,
-    name_category:req.query.name_category.toLowerCase()
+    name_category: req.query.name_category.toLowerCase()
   }
   var cat = new Cat(item);
   console.log(item);
-  cat.save(function (err, createdTodoObject) {  
+  cat.save(function (err, createdTodoObject) {
     if (err) {
-        res.send(err);
+      res.send(err);
     }
     res.send(createdTodoObject);
   });
 });
 
-router.get('/listCategory', function(req, res, next) {  
-  Cat.find(function (err, categories) {  
+router.get('/listCategory', function (req, res, next) {
+  Cat.find(function (err, categories) {
     if (err) {
-        res.status(500).send(err)
+      res.status(500).send(err)
     } else {
-        res.send(categories);
+      res.send(categories);
     }
   });
 });
 
-router.get('/findCategoryById', function(req, res, next) {  
+router.get('/findCategoryById', function (req, res, next) {
   var idcat = req.query.id_category;
-  Cat.find({"id_category": idcat}, function (err, categories) {  
+  Cat.find({ "id_category": idcat }, function (err, categories) {
     if (err) {
-        res.status(500).send(err)
+      res.status(500).send(err)
     } else {
-        // send the list of all people in database with name of "John James" and age of 36
-        // Very possible this will be an array with just one Person object in it.
-        res.send(categories);
+      // send the list of all people in database with name of "John James" and age of 36
+      // Very possible this will be an array with just one Person object in it.
+      res.send(categories);
     }
   });
 });
